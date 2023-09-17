2ac5f6e8b136f595c88def739c6371c0376b05cb
shahin <sh.bozorgi@phinix.com>
2023-09-18 00:56:38 +0330

    change composer name

---
+++ tests/LogViewerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Falcon\Logviewer;
+
+use Tests\TestCase;
+
+class LogViewerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
